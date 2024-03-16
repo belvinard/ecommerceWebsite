fdcab01c42001fc2762aebbd6b46c2e84e2e28f9
belviard <belvinard97@gmail.com>
2024-03-16 22:22:24 +0100

    common_function.php updated

--- functions/common_function.php
+++ functions/common_function.php
@@ -52,6 +52,54 @@
         }
     }
 
+    // Getting all products
+    function get_all_products(){
+        global $con;
+        if(!isset($_GET['category'])){
+            if(!isset($_GET['brand'])){
+                try {
+                // Prepare and execute the query to fetch the products
+                //$select_query = "SELECT * FROM `productsInserted` order by products_title"; 
+                $select_query = "SELECT * FROM `productsInserted` order by rand()";
+                $stmt = $con->prepare($select_query);
+                $stmt->execute();
+
+                    // Fetch the first row (product) from the result set
+                    //$row = $stmt->fetch(PDO::FETCH_ASSOC);
+                    while($row = $stmt->fetch(PDO::FETCH_ASSOC)){
+                
+                        // Access individual fields
+                        $products_id = $row['products_id'];
+                        $products_title = $row['products_title'];
+                        $products_description = $row['products_description'];
+                        $categories_id = $row['categories_id'];
+                        $brands_id = $row['brands_id'];
+                        $products_image1 = $row['products_image1'];
+                        $products_price = $row['products_price'];
+
+                        echo "<div class='col-md-4 mb-2'>
+                        <div class='card'>
+                            <img src='./admin_area/product_images/$products_image1' class='card-img-top' alt='$products_title'>
+                            <div class='card-body'>
+                                <h5 class='card-title'>$products_title</h5>
+                                <p class='card-text'>$products_description</p>
+                                <a href='#' class='btn btn-info'>Add to cart</a>
+                                <a href='#' class='btn btn-secondary'>View more</a>
+                            </div>
+                        </div>
+                    </div>";
+
+
+                    }
+
+                } catch (PDOException $e) {
+                // Handle any errors that occur during the execution of the query
+                echo "Error: " . $e->getMessage();
+                }
+            }
+        }
+    }
+
     // Get unique categories 
     function get_unique_categories(){
         global $con;
